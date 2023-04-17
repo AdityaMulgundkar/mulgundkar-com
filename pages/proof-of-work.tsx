1f4d260fcff1d3cd9115eb701391e0db235dfe10
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2023-04-17 11:45:19 +0530

    Updated PoW

--- pages/proof-of-work.tsx
+++ pages/proof-of-work.tsx
@@ -6,6 +6,8 @@ import YoutubeEmbed from "@/components/YoutubeEmbed";
 import { cx } from "@/lib/utils";
 import { Proof } from "@/components/Proof";
 import Link from "next/link";
+import { Check, X } from "react-feather";
+import { LogoTooltip } from "@/components/LogoTooltip";
 
 interface HomeProps {
   posts: Array<MDXFrontMatter>;
@@ -39,8 +41,7 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
       >
         <Proof
           firstItem={true}
-          title="Embedded system based Fault Tolerant Control and Autonomous
-            Navigation of a UAV"
+          title="Building Fault Tolerant UAVs using Machine Learning, Fault Analysis and Control Theory"
           category="MS Thesis, Robotics, Fault Tolerance in UAVs"
         >
           <YoutubeEmbed embedId="z1vDjloeLiw" />
@@ -54,6 +55,58 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
             alt="Hexa frame"
             className={cx("w-full")}
           />
+          <p className="mb-4 block font-sans text-base font-light leading-relaxed antialiased">
+            Following types of UAV faults were detected and corrected:
+          </p>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th>Fault Type</th>
+                <th>Description</th>
+                <th>QuadCopter</th>
+                <th>HexaCopter</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Motor Faults</td>
+                <td>
+                  Loss of Efficiency (LoE)
+                  <br />
+                  What happens when a motor works at a lower efficiency
+                </td>
+                <td><Check width=".9em" /></td>
+                <td><Check width=".9em" /></td>
+              </tr>
+              <tr>
+                <td>Motor Failures</td>
+                <td>Loss of Actuation (LoA)
+                  <br />
+                  What happens when a motor stops working completely
+                </td>
+                <td><X width=".9em" /></td>
+                <td><Check width=".9em" /></td>
+              </tr>
+              <tr>
+                <td>Motor Locking</td>
+                <td>What happens when a motor gets stuck at some RPM</td>
+                <td><X width=".9em" /></td>
+                <td><X width=".9em" /></td>
+              </tr>
+              <tr>
+                <td>Propeller Faults</td>
+                <td>What happens when a propeller chips off</td>
+                <td><Check width=".9em" /></td>
+                <td><Check width=".9em" /></td>
+              </tr>
+              <tr>
+                <td>Sensor Faults</td>
+                <td>What happens when a sensor stops working</td>
+                <td><Check width=".9em" /></td>
+                <td><Check width=".9em" /></td>
+              </tr>
+            </tbody>
+          </table>
         </Proof>
         <Proof
           title="Design, fabrication & analysis of a magnetostrictive test-bed running in a lower-earth orbit"
@@ -88,6 +141,16 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
             </li>
             <li>
               <a href="https://pub.dev/opencv" target="_blank" rel="noreferrer">Flutter OpenCV</a>
+            </li>
+            <li>
+              <a href="https://github.com/AdityaMulgundkar/OpenCV-nuget" target="_blank" rel="noreferrer">
+                OpenCV Nuget
+              </a>
+              <br />
+              My work on easing your workflow for&nbsp;
+              <a href="https://summerofcode.withgoogle.com/archive/2021/projects/6006857948200960" target="_blank" rel="noreferrer">
+                OpenCV on Windows
+              </a> using the NuGet Package manager.
             </li>
             <li>
               <a href="https://pub.dev/wallpaper_manager" target="_blank" rel="noreferrer">Wallpaper Manager</a>
@@ -142,20 +205,23 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
               <a href="https://wedeasy.in" target="_blank" rel="noreferrer">WedEasy</a>
             </li>
             <li>
+              <a href="https://solcodes.vercel.app" target="_blank" rel="noreferrer">SolCodes</a>
+            </li>
+            <li>
               <a href="https://www.guildcap.com/" target="_blank" rel="noreferrer">Guild Capital</a>
             </li>
             <li>
               <a href="https://www.atidiv.com/" target="_blank" rel="noreferrer">Atidiv</a>
             </li>
             <li>
-              <a href="https://www.argylesp.com/" target="_blank" rel="noreferrer">ArgyleSP</a>
+              <a href="https://www.argylesp.com/" target="_blank" rel="noreferrer">Argyle Search Partners</a>
             </li>
             <li>
               <a href="https://www.blackwatch.io/" target="_blank" rel="noreferrer">BlackWatch Advisors</a>
             </li>
           </ul>
         </Proof>
-      </Page>
+      </Page >
     </>
   );
 };
